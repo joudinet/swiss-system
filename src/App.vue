27ad7db7d22366db14efad2fdf3b84fd1602c6a5
Johan Oudinet <johan.oudinet@gmail.com>
2020-07-24 19:59:34 +0200

    Use Roboto font and provide backup fonts.

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Poppins', sans-serif;
+  font-family: 'Roboto', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
